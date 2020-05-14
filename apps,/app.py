7dbb14a6b6e3ead5cf613a91f1a202e75624f271
spogoff <samin.pogoff@gmail.com>
2020-05-14 12:25:58 -0400

    added python code for flask in app.py

---
+++ apps,/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#set flask route for main page
+@app.route("/")
+
+#function for PyMongo to connect to mongoDB collection mars and return it in an html template using index.html
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+#set flask route for scrape page
+@app.route("/api/v1.0/scrape")
+
+# function to scrape all data using scraping code and update it into a Mongo databse
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
+
+#run flask
+if __name__ == '__main__":
+app.run()
